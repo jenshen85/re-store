e813c57ce2819aae23eaae81cc3132df3395bf6b
Eugene <eshereverov@gmail.com>
2019-04-25 23:23:43 +0300

    codereview reducer, add module booklist and shopping cart

---
+++ src/reducers/book-list.js
@@ -0,0 +1,40 @@
+import {
+  FETCH_BOOKS_FAILURE,
+  FETCH_BOOKS_REQUEST,
+  FETCH_BOOKS_SUCCESS,
+  } from '../constants';
+
+const updateBookList = (state, action) => {
+  if(state === undefined) {
+    return {
+      books: [],
+      loading: true,
+      error: null,
+    }
+  }
+
+  switch(action.type) {
+    case FETCH_BOOKS_REQUEST:
+      return {
+        books: [],
+        loading: true,
+        error: null,
+      }
+    case FETCH_BOOKS_SUCCESS:
+      return {
+        books: action.payload,
+        loading: false,
+        error: null,
+      }
+    case FETCH_BOOKS_FAILURE:
+      return {
+        books: [],
+        loading: false,
+        error: action.payload,
+      }
+    default:
+      return state.bookList
+  }
+}
+
+export default updateBookList;
