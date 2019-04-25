a1baf377d8452a05ea7fb5d173a3b2ff3d7a9bd3
Eugene <eshereverov@gmail.com>
2019-04-25 23:46:29 +0300

    add middleware

--- src/store.js
+++ src/store.js
@@ -1,7 +1,20 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const logMiddleware = (store) => (dispatch) => (action) => {
+  console.log(action.type);
+  return dispatch(action);
+}
+
+const stringMiddleware = (store) => (dispatch) => (action) => {
+  if(typeof action === 'string') {
+    return dispatch({type: action});
+  }
+
+  return dispatch(action)
+}
+
+const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
 
 export default store;
